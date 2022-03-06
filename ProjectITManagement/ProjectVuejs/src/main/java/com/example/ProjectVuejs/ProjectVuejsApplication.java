c02e151d7c1c064ba5a2299343e0b62f3c9ad130
bcdai321996 <67669250+bcdai321996@users.noreply.github.com>
2022-03-06 23:54:19 +0700

    init

---
+++ ProjectITManagement/ProjectVuejs/src/main/java/com/example/ProjectVuejs/ProjectVuejsApplication.java
@@ -0,0 +1,15 @@
+package com.example.ProjectVuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ProjectVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVuejsApplication.class, args);
+	}
+
+}
